c719bd94c1895c975be9027cd2343b4536565f9d
Youssefeslamm <161878370+Youssefeslamm@users.noreply.github.com>
2026-03-22 05:48:54 +0200

    feat(user-service): implement S1-F1 search users (55-1069)

--- user-service/src/main/java/com/team35/freelance/user/repository/UserRepository.java
+++ user-service/src/main/java/com/team35/freelance/user/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.team35.freelance.user.repository;
 
 import com.team35.freelance.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +15,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPhone(String phone);
 
     boolean existsByEmail(String email);
+
+    @Query(value = """
+    SELECT * FROM users u
+    WHERE (:name IS NULL OR u.name ILIKE CONCAT('%', :name, '%'))
+      AND (:email IS NULL OR u.email ILIKE CONCAT('%', :email, '%'))
+      AND (:role IS NULL OR u.role = CAST(:role AS user_role_enum))
+""", nativeQuery = true)
+    List<User> searchUsers(
+            @Param("name") String name,
+            @Param("email") String email,
+            @Param("role") String role
+    );
 }
