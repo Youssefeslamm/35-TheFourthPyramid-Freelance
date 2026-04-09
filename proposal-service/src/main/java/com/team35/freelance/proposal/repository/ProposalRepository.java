b63fff07c8dcba5768fc7fcc9bb0ad61413f9876
Shahd <161853211+Shahd-Moghazy@users.noreply.github.com>
2026-04-09 05:48:06 +0200

    fix ProposalRepository.java
    removed extra brackets

--- proposal-service/src/main/java/com/team35/freelance/proposal/repository/ProposalRepository.java
+++ proposal-service/src/main/java/com/team35/freelance/proposal/repository/ProposalRepository.java
@@ -35,11 +35,3 @@ public interface ProposalRepository extends JpaRepository<Proposal, Long> {
 
 }
 
-
-import com.team35.freelance.proposal.model.Proposal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProposalRepository extends JpaRepository<Proposal, Long> {
-}
