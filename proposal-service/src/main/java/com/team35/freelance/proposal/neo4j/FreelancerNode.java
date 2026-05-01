15d4da007ecfa0c07f8e25efdc893c13edc3d691
youssefNaizy <niazyyoussef28@gmail.com>
2026-04-30 17:14:51 -0700

    feat(cc): add FreelancerNode Neo4j entity (55-12460)

---
+++ proposal-service/src/main/java/com/team35/freelance/proposal/neo4j/FreelancerNode.java
@@ -0,0 +1,37 @@
+package com.team35.freelance.proposal.neo4j;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node("Freelancer")
+public class FreelancerNode {
+
+    @Id
+    private Long userId;
+
+    private String name;
+
+    public FreelancerNode() {
+    }
+
+    public FreelancerNode(Long userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
