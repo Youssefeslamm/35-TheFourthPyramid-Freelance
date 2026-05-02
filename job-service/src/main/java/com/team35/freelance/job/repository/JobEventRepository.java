853ccbe09755971bb0473a9d1286d70633ed36fa
Youssefeslamm <161878370+Youssefeslamm@users.noreply.github.com>
2026-05-02 22:51:56 +0300

    Revert "Feat/cc/dp 2 mongo event entities/55 12460"

--- job-service/src/main/java/com/team35/freelance/job/repository/JobEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team35.freelance.job.repository;
-
-import com.team35.freelance.job.event.JobEvent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JobEventRepository extends MongoRepository<JobEvent, String> {
-}
--- wallet-service/src/main/java/com/team35/freelance/wallet/repository/PayoutAuditEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team35.freelance.wallet.repository;
-
-import com.team35.freelance.wallet.event.PayoutAuditEvent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PayoutAuditEventRepository extends MongoRepository<PayoutAuditEvent, String> {
-}
