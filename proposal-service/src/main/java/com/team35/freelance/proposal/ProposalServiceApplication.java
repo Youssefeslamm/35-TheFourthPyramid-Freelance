7a41210201e13788f3dcb6af04419609d5e9897e
Youssefeslamm <161878370+Youssefeslamm@users.noreply.github.com>
2026-05-14 01:09:49 +0300

    setup OpenFeign infrastructure for M3 communication

--- proposal-service/src/main/java/com/team35/freelance/proposal/ProposalServiceApplication.java
+++ proposal-service/src/main/java/com/team35/freelance/proposal/ProposalServiceApplication.java
@@ -3,11 +3,13 @@ package com.team35.freelance.proposal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 
 @EnableCaching
+@EnableFeignClients
 @SpringBootApplication
 @EnableNeo4jRepositories(basePackages = "com.team35.freelance.proposal.repository")
 @EnableJpaRepositories(basePackages = "com.team35.freelance.proposal.repository")
