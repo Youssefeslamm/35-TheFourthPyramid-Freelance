7de09c6e7fbc41db8b75c301bf91eea464a812b5
moheshhamm <mohamed_shehata5641@yahoo.com>
2026-05-14 04:10:27 +0300

    feat(infra): add M3 gateway and Kubernetes scaffold (55-8017)

---
+++ api-gateway/src/main/java/com/team35/freelance/gateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.team35.freelance.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
