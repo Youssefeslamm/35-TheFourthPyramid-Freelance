b4e60ce1c404c751e204fc6c28a167303885d09d
Shahd Ayman <aymanshahd46@gmail.com>
2026-05-16 23:21:46 +0300

    Revert "Merge pull request #164 from Youssefeslamm/fix/postgres-init-db/55-8017"
    This reverts commit 5f39b29cf88b4587ced7d0843619bd6d816490f8, reversing
    changes made to 36ab795373f3d12cf4f4b80aa6d36fc47bbf37a4.

--- user-service/src/main/java/com/team35/freelance/user/config/RabbitExchangeConfig.java
+++ user-service/src/main/java/com/team35/freelance/user/config/RabbitExchangeConfig.java
@@ -1,7 +1,6 @@
 package com.team35.freelance.user.config;
 
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +10,5 @@ public class RabbitExchangeConfig {
     @Bean
     public TopicExchange userEventsExchange() {
         return new TopicExchange("user.events");
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 }
